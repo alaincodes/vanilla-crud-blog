e2e59243d3e18dc73dea1e867bd1fd4dbad83e96
Alain <alaint77@gmail.com>
2019-07-30 16:34:58 +0200

    audit fix..

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -8,121 +8,14 @@ addPostBtn.addEventListener("click", () => {
   addPostModal.style.display = "block";
 });
 
-function closeModal() {
-  closeModalBtn.addEventListener("click", () => {
-    addPostModal.style.display = "none";
-  });
-}
-closeModal();
+closeModalBtn.addEventListener("click", () => {
+  addPostModal.style.display = "none";
+});
 
 addPostModal.addEventListener("click", event => {
   if (event.target == addPostModal) {
     addPostModal.style.display = "none";
   }
-});
-
-class Blog {
-  constructor(title, text) {
-    this.title = title;
-    this.text = text;
-  }
-}
-
-class UI {
-  static displayBlogs() {
-    const blogs = StoreBlog.getBlogs();
-    blogs.forEach(blog => UI.addBlogToList(blog));
-  }
-
-  static addBlogToList(blog) {
-    const list = document.querySelector("#post-container");
-
-    const row = document.createElement("div");
-
-    row.innerHTML = `
-    <div class="blog-post-card">
-    <span id="delete-post-btn" class="close-btn delete">&times;</span>
-    <h2>${blog.title}</h2>
-    <p>${blog.text}</p>
-    <div class="footer-card">
-      <button>read</button>
-      <p class="edit-post">Edit..</p>
-    </div>
-  </div>
-      `;
-
-    list.appendChild(row);
-  }
-
-  static deleteBlog(el) {
-    if (el.classList.contains("delete")) {
-      el.parentElement.parentElement.remove();
-    }
-  }
-
-  static clearFields() {
-    document.querySelector("#post-title-value").value = "";
-    document.querySelector("#post-text-value").value = "";
-    addPostModal.style.display = "none";
-  }
-}
-
-class StoreBlog {
-  static getBlogs() {
-    let blogs;
-    if (localStorage.getItem("blogs") === null) {
-      blogs = [];
-    } else {
-      blogs = JSON.parse(localStorage.getItem(blogs));
-    }
-    return blogs;
-  }
-
-  static addBlog(blog) {
-    const blogs = StoreBlog.getBlogs();
-    blogs.push(blog);
-    localStorage.setItem("blogs", JSON.stringify(blogs));
-  }
-
-  static removeBlog(title) {
-    const blogs = StoreBlog.getBlogs();
-
-    blogs.forEach((blog, index) => {
-      if (blog.title === title) {
-        blogs.splice(index, 1);
-      }
-    });
-    localStorage.setItem("blogs", JSON.stringify(blogs));
-  }
-}
-
-// Event: Display Books
-document.addEventListener("DOMContentLoaded", UI.displayBlogs);
-
-// Event: Add a Book
-document.querySelector("#form-post").addEventListener("submit", e => {
-  // Prevent actual submit
-  e.preventDefault();
-
-  // Get form values
-  const title = document.querySelector("#post-title-value").value;
-  const text = document.querySelector("#post-text-value").value;
-
-  // Instatiate blog
-  const blog = new Blog(title, text);
-
-  // Add blog to UI
-  UI.addBlogToList(blog);
-
-  // Add book to store
-  StoreBlog.addBlog(blog);
-
-  UI.clearFields();
-});
-
-document.querySelector("#post-container").addEventListener("click", e => {
-  // Remove book from UI
-  UI.deleteBlog(e.target);
 });
 
 // Fetch Blog Posts link
@@ -132,7 +25,7 @@ document.querySelector("#post-container").addEventListener("click", e => {
 //     return result.json();
 //   })
 //   .then(data => {
-//     const PostGridSection = document.getElementById("post-container");
+//     const PostGridSection = document.querySelector("#post-container");
 //     let displayPost = "";
 //     data.forEach(post => {
 //       displayPost += `
